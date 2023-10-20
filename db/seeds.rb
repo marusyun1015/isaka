7bb36df9813304e0298cbaf8472c2b662c73206f
isaka <circlebridge.syun@gmail.com>
2023-10-20 23:36:33 +0900

    Webサービス　DBと接続

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 User.create!(
-    email: 'b@ababababa ',
+    email: 'b@ababababab ',
     admin: 'true',
     name: 'a',
     password: 'aaaaaa'
